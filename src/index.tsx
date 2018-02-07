1ee4a699b5319aac56c184a45907a71358b9a30d
Vincent van den Braken <vincent@vandenbraken.com>
2018-02-07 19:28:06 +0100

    Initial setup

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
